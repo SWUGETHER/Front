74c0ebe2290f946be836a9ec11a163b535250426
Yusun Choi <nsuy.ch@gmail.com>
2023-06-07 12:39:45 +0900

    fix: Modify store (#10)

--- redux/store.js
+++ redux/store.js
@@ -5,11 +5,11 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import userReducer from "./reducers/userReducer";
 import settingReducer from "./reducers/settingReducer";
 
-const rootReducer = combineReducers({ userReducer, settingReducer });
 const persistConfig = {
   key: "root",
   storage: AsyncStorage,
 };
+const rootReducer = combineReducers({ userReducer, settingReducer });
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
