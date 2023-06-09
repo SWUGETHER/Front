1f9121c7391947db69f2a4527620e71ed3296323
Yusun Choi <nsuy.ch@gmail.com>
2023-06-10 02:18:54 +0900

    feat: Add addPost API (#10)

--- screens/Content/ContentHeader.js
+++ screens/Content/ContentHeader.js
@@ -1,52 +1,59 @@
 import { useNavigation } from "@react-navigation/native";
-import React from 'react';
-import { Pressable,StyleSheet,View } from "react-native";
+import React from "react";
+import {
+  Pressable,
+  StyleSheet,
+  View,
+  TouchableWithoutFeedback,
+  Keyboard,
+} from "react-native";
 import Icon from "react-native-vector-icons/MaterialIcons";
 import WriteCircleButton from "../../UI/WriteCircleButton";
 
-
-function ContentHeader({onSave}){
-    const navigation=useNavigation();
-    const onGoBack=()=>{
-        navigation.pop();
-    };
-    return(
-        <View style={styles.contain}>
-            <View style={styles.iconBtnWrap}>
-                <WriteCircleButton
-                    onPress={onGoBack}
-                    name="arrow-back"
-                    color="#000000" 
-                />
-            </View>
-            
-            <View style={styles.btns}>
-                    <WriteCircleButton
-                        name="delete-forever"
-                        color="#000000"
-                        hasMarginRight 
-                    />
-                    <WriteCircleButton 
-                        name="check"
-                        color="#000000" 
-                        onPress={onSave}
-                    />
-            </View>
+function ContentHeader({ onSave }) {
+  const navigation = useNavigation();
+  const onGoBack = () => {
+    navigation.pop();
+  };
+  return (
+    <TouchableWithoutFeedback
+      onPress={() => {
+        Keyboard.dismiss();
+      }}
+    >
+      <View style={styles.contain}>
+        <View style={styles.iconBtnWrap}>
+          <WriteCircleButton
+            onPress={onGoBack}
+            name="arrow-back"
+            color="#000000"
+          />
         </View>
-    );
-};
 
-const styles=StyleSheet.create({
-    contain:{
-        height: 48,
-        paddingHorizontal: 8,
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'space-between',
-    },
-    btns:{
-        flexDirection:'row',
-        alignItems:'center',
-    },
+        <View style={styles.btns}>
+          <WriteCircleButton
+            name="delete-forever"
+            color="#000000"
+            hasMarginRight
+          />
+          <WriteCircleButton name="check" color="#000000" onPress={onSave} />
+        </View>
+      </View>
+    </TouchableWithoutFeedback>
+  );
+}
+
+const styles = StyleSheet.create({
+  contain: {
+    height: 48,
+    paddingHorizontal: 8,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  btns: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
 });
 export default ContentHeader;
--- screens/Content/ContentWrite.js
+++ screens/Content/ContentWrite.js
@@ -1,34 +1,43 @@
-import React, { useState, useContext } from 'react';
-import { useNavigation } from '@react-navigation/native';
-import { StyleSheet, KeyboardAvoidingView, Platform } from 'react-native';
-import ContentHeader from './ContentHeader';
-import ContentEditor from './ContentEditor';
-import LogContext from './context/LogContext';
-
+import React, { useState, useContext } from "react";
+import { useNavigation } from "@react-navigation/native";
+import { StyleSheet, KeyboardAvoidingView, Platform } from "react-native";
+import ContentHeader from "./ContentHeader";
+import ContentEditor from "./ContentEditor";
+import LogContext from "./context/LogContext";
+import addPost from "../../API/post/addPost";
+import { useSelector, useDispatch } from "react-redux";
 
 function ContentWrite() {
-  const [title, setTitle] = useState('');
-  const [body, setBody] = useState('');
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const [images, setImages] = useState([]);
 
   const navigation = useNavigation();
+
+  const dispatch = useDispatch();
+  const state = useSelector((state) => state);
 
   // LogContext에서 onCreate 함수 가져오기
   const logContext = useContext(LogContext);
   const { onCreate } = logContext;
 
-  const onSave = () => {
+  const onSave = async () => {
     onCreate({
       title,
       body,
+      images,
       date: new Date().toISOString(),
     });
+
+    await addPost(state, dispatch, title, body, images);
+
     navigation.pop();
   };
 
   return (
     <KeyboardAvoidingView
       style={styles.avoidingView}
-      behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+      behavior={Platform.OS === "ios" ? "padding" : undefined}
     >
       <ContentHeader onSave={onSave} />
       <ContentEditor
@@ -36,6 +45,7 @@ function ContentWrite() {
         body={body}
         onChangeTitle={setTitle}
         onChangeBody={setBody}
+        onChangeImage={setImages}
       />
     </KeyboardAvoidingView>
   );
@@ -44,7 +54,7 @@ function ContentWrite() {
 const styles = StyleSheet.create({
   contain: {
     flex: 1,
-    backgroundColor: '#D9D9D9',
+    backgroundColor: "#D9D9D9",
   },
   avoidingView: {
     flex: 1,
